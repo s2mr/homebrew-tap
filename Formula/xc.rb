89a6a0eadab2b5b6b427a7cfca89a35b55635ef3
Kazumasa Shimomura <shimomura_kazumasa@cyberagent.co.jp>
2023-01-11 23:26:04 +0900

    Fix

--- Formula/xc.rb
+++ Formula/xc.rb
@@ -1,26 +1,16 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 class Xc < Formula
-  desc ""
-  homepage ""
+  desc "Open your xcode project with Xcode of specific version"
+  homepage "https://github.com/s2mr/xc"
   url "https://github.com/s2mr/xc/releases/download/0.0.2/xc.zip"
-  sha256 "902b210171064d43e036a103a0450a741fee564989563735d0d8f33a1b558477"
-  license ""
+  sha256 "dbc05bd9bb6f52bfc051b9a9a10e9dc2934001d952ce66cac9fef805e4f8782d"
+  license "MIT License"
 
   def install
     bin.install 'xc'
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test xc`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
   end
 end
