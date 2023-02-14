71c611dedd739186917e5f91d883b00251153b14
Kazumasa Shimomura <shimomura_kazumasa@cyberagent.co.jp>
2023-02-14 14:08:39 +0900

    Bump to 0.2.1 for L10nLint

--- Formula/l10nlint.rb
+++ Formula/l10nlint.rb
@@ -1,16 +1,16 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 class L10nlint < Formula
-    desc "Lint tool for Localizable.strings"
-    homepage "https://github.com/s2mr/L10nLint"
-    url "https://github.com/s2mr/L10nLint/releases/download/0.0.1/l10nlint.zip"
-    sha256 "c69af43d49fdf9a4cd32f7da846c3a08625ce77bf75e86fe1d78ea047ec019c5"
-    license "MIT"
+  desc "Lint tool for Localizable.strings"
+  homepage "https://github.com/s2mr/L10nLint"
+  url "https://github.com/s2mr/L10nLint/releases/download/0.2.1/l10nlint.zip"
+  sha256 "d6925c610cadbeeddfbb58c1036c92824b9bfaf9a417e30674fc9766b1b88877"
+  license "MIT"
 
-    def install
-      bin.install 'l10nlint'
-    end
-
-    test do
-    end
+  def install
+    bin.install 'l10nlint'
   end
+
+  test do
+  end
+end
